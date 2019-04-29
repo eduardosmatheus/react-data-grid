c9b483c45b273bbd4b268e461f22a4c3fe532e2f
eduardosmatheus <matheus-edu@outlook.com>
2019-04-29 16:17:15 -0300

    Remove unused import.

--- packages/react-data-grid-addons/src/menu/index.js
+++ packages/react-data-grid-addons/src/menu/index.js
@@ -1,11 +1,10 @@
 import {
   ContextMenu,
   MenuItem,
-  monitor,
   SubMenu,
   connectMenu as connect,
   ContextMenuTrigger
 } from 'react-contextmenu';
 import MenuHeader from './MenuHeader';
 
-export { ContextMenu, MenuHeader, MenuItem, monitor, SubMenu, connect, ContextMenuTrigger };
+export { ContextMenu, MenuHeader, MenuItem, SubMenu, connect, ContextMenuTrigger };
