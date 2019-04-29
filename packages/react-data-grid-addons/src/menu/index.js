6b80048d301d2e7052cc7ab59931f328fc378e7a
eduardosmatheus <matheus-edu@outlook.com>
2019-04-29 15:55:48 -0300

    Fix wrong import

--- packages/react-data-grid-addons/src/menu/index.js
+++ packages/react-data-grid-addons/src/menu/index.js
@@ -1,4 +1,11 @@
-import { ContextMenu, MenuItem, monitor, SubMenu, connect, ContextMenuTrigger } from 'react-contextmenu';
+import {
+  ContextMenu,
+  MenuItem,
+  monitor,
+  SubMenu,
+  connectMenu as connect,
+  ContextMenuTrigger
+} from 'react-contextmenu';
 import MenuHeader from './MenuHeader';
 
 export { ContextMenu, MenuHeader, MenuItem, monitor, SubMenu, connect, ContextMenuTrigger };
