6e893f10b6f71807448ae44c4d52525ebab22083
Daniel André da Silva <dap1995@gmail.com>
2019-09-12 13:43:17 -0300

    Allow to sort object with value key

--- packages/react-data-grid-addons/src/data/RowSorter.js
+++ packages/react-data-grid-addons/src/data/RowSorter.js
@@ -9,11 +9,20 @@ export const comparer = (a, b) => {
   return 0;
 };
 
+const parseValue = (value) => {
+  // Workaround 'cause typeof null is equal object.
+  return (value && typeof value === 'object')
+    ? value.value
+    : value;
+};
+
 const sortRows = (rows, sortColumn, sortDirection) => {
-  const retriever = getMixedTypeValueRetriever(isImmutableCollection(rows));
+  const { getValue } = getMixedTypeValueRetriever(isImmutableCollection(rows));
   const sortDirectionSign = sortDirection === 'ASC' ? 1 : -1;
   const rowComparer = (a, b) => {
-    return sortDirectionSign * comparer(retriever.getValue(a, sortColumn), retriever.getValue(b, sortColumn));
+    const aValue = parseValue(getValue(a, sortColumn));
+    const bValue = parseValue(getValue(b, sortColumn));
+    return sortDirectionSign * comparer(aValue, bValue);
   };
   if (sortDirection === 'NONE') {
     return rows;
